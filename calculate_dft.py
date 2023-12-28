a8f87bd63bc51c63bd615ecd37fecebd0927f71d
lifrank1765 <148572657+lifrank1765@users.noreply.github.com>
2023-12-28 17:04:36 -0500

    Update calculate_dft.py

--- calculate_dft.py
+++ calculate_dft.py
@@ -28,31 +28,10 @@ def calculate_dft_coefficients(signal):
     dft_coefficients = np.sum(signal * e, axis=1)
     return dft_coefficients
 
-# Example usage:
-# Create a simple signal (e.g., a sine wave)
-fs = 1000  # Sampling frequency
-t = np.arange(0, 1, 1/fs)  # Time vector
-frequency = 5  # Frequency of the sine wave
-signal = np.sin(2 * np.pi * frequency * t)
-
-# Calculate DFT coefficients using the custom function
-dft_coefficients_custom = calculate_dft_coefficients(signal)
-
-# Display the original signal and its DFT
-plt.subplot(2, 1, 1)
-plt.plot(t, signal)
-plt.title('Original Signal')
-
-plt.subplot(2, 1, 2)
-plt.stem(np.abs(dft_coefficients_custom))
-plt.title('DFT Coefficients (Custom)')
-
-plt.show()
 
 
 
-
-def calculate_dft_coefficients(signal):
+def get_conjugate_transpose(freq,sampling_rate):
     """
     Calculate the DFT coefficients for a given signal.
 
@@ -62,54 +41,34 @@ def calculate_dft_coefficients(signal):
     Returns:
     - dft_coefficients: DFT coefficients (complex numpy array)
     """
-    N = len(signal)
+    N = sampling_rate
     n = np.arange(N)
     k = n.reshape((N, 1))
     
     # Constructing the matrix of complex exponentials
     e = np.exp(-2j * np.pi * k * n / N)
-    print(e.shape)
-    
-    # Calculating DFT coefficients
-    dft_coefficients = np.sum(signal * e, axis=1)
-    #a = signal * e
-    #print(a[:,5])
-    print(np.abs(dft_coefficients[:10]))
-    print(np.abs(np.sum(signal * e[:,5])))
-    plt.plot( e[:,10].real)
-    plt.plot( e[:,10].imag)
-    
-    plt.show()
-    #print(signal * e[:,5].T)
-    #print(e[:,5])
-    
-    return dft_coefficients
+    return e[:,freq]
+
+
+
+
 
 print("test")
 # Example usage:
 # Create a simple signal (e.g., a sine wave)
 fs = 1000  # Sampling frequency
 t = np.arange(0, 1, 1/fs)  # Time vector
-frequency = 5  # Frequency of the sine wave
+frequency = 10  # Frequency of the sine wave
 signal = np.sin(2 * np.pi * frequency * t)
 
-# Calculate DFT coefficients using the custom function
-dft_coefficients_custom = calculate_dft_coefficients(signal)
-#print(np.abs(dft_coefficients_custom))
+conj_10 = get_conjugate_transpose(10,1000)
 
-
+conj_20 = get_conjugate_transpose(20,1000)
 
 plt.plot(t, signal)
 plt.title('Original Signal')
 plt.show()
 
-# Display the original signal and its DFT
-plt.subplot(2, 1, 1)
-plt.plot(t, signal)
-plt.title('Original Signal')
-
-plt.subplot(2, 1, 2)
-plt.stem(np.abs(dft_coefficients_custom))
-plt.title('DFT Coefficients (Custom)')
-
+plt.plot(np.abs(np.convolve(signal,conj_10[:400],'valid'))[::100])
+plt.plot(np.abs(np.convolve(signal,conj_20[:400],'valid'))[::100])
 plt.show()
